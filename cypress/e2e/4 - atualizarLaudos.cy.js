b4b3a489a5042fde6b6b58905d45bd29149af24c
Ricael Pereira Barbosa <85632605+RicaelBarbosa@users.noreply.github.com>
2023-09-28 14:06:57 -0300

    adicionando json em atualizar laudos

--- cypress/e2e/4 - atualizarLaudos.cy.js
+++ cypress/e2e/4 - atualizarLaudos.cy.js
@@ -2,8 +2,8 @@ describe('acessar-site', () => {
     it('acessar o site', () => {
         cy.visit("https://editor-homologacao.ilaudo.com");
         cy.title().should('eq', 'iLaudo Editor - Sistema de criação de laudos')
-        cy.get('input[type=email]').type('zscanrick@outlook.com')
-        cy.get('input[type=password]').type('123456')
+        cy.get('input[type=email]').type(usuarioSenha.dados.email)
+        cy.get('input[type=password]').type(usuarioSenha.dados.senha)
         cy.get('button[type="submit"]').click()
         cy.get('.nav > .btn').click()
 
